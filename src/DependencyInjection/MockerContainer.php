484f2baf7478d5b14af25f1e8e768bc2c483d008
Loïc Frémont <lc.fremont@gmail.com>
2024-09-09 16:19:56 +0200

    Fix Symfony 4 & 5 support

--- src/DependencyInjection/MockerContainer.php
+++ src/DependencyInjection/MockerContainer.php
@@ -4,66 +4,134 @@ namespace PSS\SymfonyMockerContainer\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Container;
 
-class MockerContainer extends Container
-{
-    /**
-     * @var array $mockedServices
-     */
-    static private $mockedServices = array();
-
-    /**
-     * Takes an id of the service as the first argument.
-     * Any other arguments are passed to the Mockery factory.
-     *
-     * @return \Mockery\Mock
-     */
-    public function mock()
+if (null === (new \ReflectionClass(Container::class))->getMethod('get')->getReturnType()) {
+    class MockerContainer extends Container
     {
-        $arguments = func_get_args();
-        $id = array_shift($arguments);
+        /**
+         * @var array $mockedServices
+         */
+        static private $mockedServices = array();
 
-        if (!$this->has($id)) {
-            throw new \InvalidArgumentException(sprintf('Cannot mock a non-existent service: "%s"', $id));
-        }
+        /**
+         * Takes an id of the service as the first argument.
+         * Any other arguments are passed to the Mockery factory.
+         *
+         * @return \Mockery\Mock
+         */
+        public function mock()
+        {
+            $arguments = func_get_args();
+            $id = array_shift($arguments);
 
-        if (!array_key_exists($id, self::$mockedServices)) {
-            self::$mockedServices[$id] = call_user_func_array(array('Mockery', 'mock'), $arguments);
-        }
+            if (!$this->has($id)) {
+                throw new \InvalidArgumentException(sprintf('Cannot mock a non-existent service: "%s"', $id));
+            }
 
-        return self::$mockedServices[$id];
-    }
+            if (!array_key_exists($id, self::$mockedServices)) {
+                self::$mockedServices[$id] = call_user_func_array(array('Mockery', 'mock'), $arguments);
+            }
 
-    /**
-     * @return null
-     */
-    public function unmock($id)
-    {
-        unset(self::$mockedServices[$id]);
-    }
-
-    public function get(string $id, int $invalidBehavior = self::EXCEPTION_ON_INVALID_REFERENCE): ?object
-    {
-        if (array_key_exists($id, self::$mockedServices)) {
             return self::$mockedServices[$id];
         }
 
-        return parent::get($id, $invalidBehavior);
-    }
-
-    public function has(string $id): bool
-    {
-        if (array_key_exists($id, self::$mockedServices)) {
-            return true;
+        /**
+         * @return null
+         */
+        public function unmock($id)
+        {
+            unset(self::$mockedServices[$id]);
         }
 
-        return parent::has($id);
-    }
+        public function get($id, $invalidBehavior = self::EXCEPTION_ON_INVALID_REFERENCE): ?object
+        {
+            if (array_key_exists($id, self::$mockedServices)) {
+                return self::$mockedServices[$id];
+            }
 
-    /**
-     * @return array
-     */
-    public function getMockedServices()
+            return parent::get($id, $invalidBehavior);
+        }
+
+        public function has($id): bool
+        {
+            if (array_key_exists($id, self::$mockedServices)) {
+                return true;
+            }
+
+            return parent::has($id);
+        }
+
+        /**
+         * @return array
+         */
+        public function getMockedServices()
+        {
+            return self::$mockedServices;
+        }
+    }
+} else {
+    class MockerContainer extends Container
     {
-        return self::$mockedServices;
+        /**
+         * @var array $mockedServices
+         */
+        static private $mockedServices = array();
+
+        /**
+         * Takes an id of the service as the first argument.
+         * Any other arguments are passed to the Mockery factory.
+         *
+         * @return \Mockery\Mock
+         */
+        public function mock()
+        {
+            $arguments = func_get_args();
+            $id = array_shift($arguments);
+
+            if (!$this->has($id)) {
+                throw new \InvalidArgumentException(sprintf('Cannot mock a non-existent service: "%s"', $id));
+            }
+
+            if (!array_key_exists($id, self::$mockedServices)) {
+                self::$mockedServices[$id] = call_user_func_array(array('Mockery', 'mock'), $arguments);
+            }
+
+            return self::$mockedServices[$id];
+        }
+
+        /**
+         * @return null
+         */
+        public function unmock($id)
+        {
+            unset(self::$mockedServices[$id]);
+        }
+
+        public function get(string $id, int $invalidBehavior = self::EXCEPTION_ON_INVALID_REFERENCE): ?object
+        {
+            if (array_key_exists($id, self::$mockedServices)) {
+                return self::$mockedServices[$id];
+            }
+
+            return parent::get($id, $invalidBehavior);
+        }
+
+        public function has(string $id): bool
+        {
+            if (array_key_exists($id, self::$mockedServices)) {
+                return true;
+            }
+
+            return parent::has($id);
+        }
+
+        /**
+         * @return array
+         */
+        public function getMockedServices()
+        {
+            return self::$mockedServices;
+        }
     }
 }
+
+
